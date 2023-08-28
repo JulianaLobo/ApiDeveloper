20ec25f9883847212aa7e50cdc78cefbffe8a67d
Juliana Lobo <sousalobojuliana@gmail.com>
2023-08-28 20:24:27 -0300

    Initial commit

---
+++ ApiDeveloper/src/test/java/com/iftm/ApiDeveloper/ApiDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.ApiDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
